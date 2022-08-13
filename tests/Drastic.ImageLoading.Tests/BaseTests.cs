df84e9bc0bd2308aeef88ca8b4deca237d5499b8
Tim Miller <innerlogic4321@gmail.com>
2022-08-12 21:51:03 -0400

    add tests

---
+++ tests/Drastic.ImageLoading.Tests/BaseTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Drastic.ImageLoading.Tests
+{
+    public class BaseTests
+    {
+        static BaseTests()
+        {
+            Images = new string[20];
+
+            for (int i = 0; i < Images.Length; i++)
+            {
+                Images[i] = GetRandomImageUrl();
+            }
+        }
+
+        protected const string RemoteImage = "https://loremflickr.com/320/240/nature?random=0";
+        protected static string[] Images { get; private set; }
+        protected static string GetRandomImageUrl() => $"https://loremflickr.com/320/240/nature?random={Guid.NewGuid()}";
+    }
+}
+
---
+++ tests/Drastic.ImageLoading.Tests/Cache/DownloadCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xunit;
+using System.Threading.Tasks;
+using Drastic.ImageLoading;
+
+namespace Drastic.ImageLoading.Tests
+{
+    public class DownloadCache : BaseTests
+    {
+    }
+}
